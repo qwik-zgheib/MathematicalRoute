7b5c6ae518ddad538d4478e392b83cb65f739168
LuiFernandoZa <142746138+LuiFernandoZa@users.noreply.github.com>
2024-02-04 04:09:39 -0500

    se avanzo el navbar

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
-import { Title } from './Title'
-
-const SearchInput = () => {
+import styles from '../styles/Navbar.module.css'
+export const SearchInput = () => {
   return (
-    <div className="items-center justify-center">
-      <input type="text" className="p-2 border bg-gray-100 hover:bg-gray-200 border-gray-300 rounded-full text-blue-900 outline-none" />
+    <div className={styles.searchi}>
+      <input type="text" className=" border bg-gray-100 hover:bg-gray-200 border-gray-300 rounded-full text-blue-900 outline-none" />
       <FontAwesomeIcon
         icon={faSearch}
         className="ml-[-25px] relative top-2 right-2 hover:bg-blue-400 hover:rounded-full p-2 text-yellow-700 hover:text-white"
@@ -17,11 +16,7 @@ const SearchInput = () => {
 
 const Menu = () => {
   return (
-    <div className="flex flex-col p-2 bg-[#D9C7A7] w-1/5">
-      <Title />
-      <p className="text-red-900">The Mathematical Route</p>
-      <SearchInput />
-
+    <div className="flex flex-col p-2 bg-[#D9C7A7] h-[100vh]">
       <div className="p-2">
         <p>Topics</p>
         <Link to="/limites">Limits</Link>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import styles from '../styles/Navbar.module.css'
 import { Title } from './Title'
 import { Outlet } from 'react-router-dom'
+import { SearchInput } from '../components/Menu'
 const Navbar = () => {
   return (
     <>
       <div className={styles.container}>
         <div className={styles.box}>
-          <Title />
+          <div className="Mode"></div>
+          <div className="title">
+            <Title />
+          </div>
+          <div className={styles.search}>
+            <SearchInput className={styles.searchi} />
+          </div>
+
           <ul className={styles.listabox}>
             <li>1</li>
             <li>2</li>
