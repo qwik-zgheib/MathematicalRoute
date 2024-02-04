ed0450e04dec1828b9aaa5642dcbee713f2a1535
LuiFernandoZa <142746138+LuiFernandoZa@users.noreply.github.com>
2024-02-04 03:20:14 -0500

    test

--- src/components/Funciones.jsx
+++ src/components/Funciones.jsx
@@ -1,9 +1,8 @@
 import style from '../styles/Funciones.module.css'
 export function Funciones() {
-    return (
-      <div className={style.container}>
-        
-        <h1>funciones</h1>
-      </div>
-    )
-  }
+  return (
+    <div className={style.container}>
+      <h1>funciones</h1>
+    </div>
+  )
+}
--- src/components/Integrales.jsx
+++ src/components/Integrales.jsx
@@ -1,8 +1,7 @@
 export function Integrales() {
-    return (
-      <div className={`flex flex-row w-full h-screen`}>
-        
-        <h1>integrales</h1>
-      </div>
-    )
-  }
+  return (
+    <div className={`flex flex-row w-full h-screen`}>
+      <h1>integrales</h1>
+    </div>
+  )
+}
