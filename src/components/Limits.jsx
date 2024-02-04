4231153e7366d53cd1ff610dca14a328986f1b45
LuiFernandoZa <142746138+LuiFernandoZa@users.noreply.github.com>
2024-02-03 23:40:24 -0500

    estilos en search

--- src/components/Limits.jsx
+++ src/components/Limits.jsx
@@ -1,7 +1,7 @@
 import Menu from "./Menu"
 export function Limits(){
     return (
-        <div>
+        <div className="flex flex-row w-full h-screen bg-slate-700">
            <Menu/>
            <h1>Limites</h1> 
         </div>
