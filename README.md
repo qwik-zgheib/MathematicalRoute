4114b54b0845d2d52bb791915f7df34fff591ca1
LuiFernandoZa <142746138+LuiFernandoZa@users.noreply.github.com>
2024-02-04 03:16:23 -0500

    fix: resolve conflicts in LF with CRLF

--- README.md
+++ README.md
@@ -1,18 +1,19 @@
-# Instalar 
+# Instalar
 
 1. github desktop
 2. node.js
-3. 
+3.
+
 ## Extensiones para VS
+
 1. Prettier
 2. eslint
 3. errorlens
 4. tailwind css <br>
 
 # Actividades
+
 **Chritian**:Codigo SSH y dominio los mejores metodos y más baratos.<br>
 **Carlos**: Investigar como poner anuncios en una pagina web.<br>
 **Luis Fernando**:Avanzar el diseño<br>
 **Isais**:Funcionalidad.<br>
-
-
--- src/components/Limits.jsx
+++ src/components/Limits.jsx
@@ -1,12 +1,10 @@
-import Menu from "./Menu"
-import styles from "../styles/Limits.module.css"
-export function Limits(){
-    return (
-        <div className={`flex flex-row w-full h-screen ${styles.container}`}>
-           <Menu/>
-           <h1>Limites</h1> 
-        </div>
-    
-    )
+import Menu from './Menu'
+import styles from '../styles/Limits.module.css'
+export function Limits() {
+  return (
+    <div className={`flex flex-row w-full h-screen ${styles.container}`}>
+      <Menu />
+      <h1>Limites</h1>
+    </div>
+  )
 }
-
