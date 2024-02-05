ab60a3ec3f799b228c6fb388979c6fb91f85a717
qwik-zgheib <qwik-zgheib@users.noreply.github.com>
2024-02-05 10:52:22 -0500

    add routes.josn and use in Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,39 +1,10 @@
 import { Link, Outlet } from 'react-router-dom'
 import styles from '../styles/Navbar.module.css'
 
+import data from '../db/routes.json'
+
 const Menu = () => {
-  //esto se llevara aun archivo externo debido a que sera extenso, en temas de modularizacion y buenas practicas.
-  const routes = [
-    {
-      name: 'Funciones',
-      path: 'functions',
-      routes: [{ name: 'Item 1', path: 'Subtema 1' }],
-    },
-    {
-      name: 'Limites',
-      path: 'limits',
-      routes: [
-        { name: 'Item 1', path: 'Subtema 1' },
-        { name: 'Item 2', path: 'Subtema 2' },
-        { name: 'Item 3', path: 'Subtema 3' },
-      ],
-    },
-    {
-      name: 'Derivadas',
-      path: 'derivatives',
-      routes: [
-        { name: 'Item 1', path: 'Subtema 1' },
-        { name: 'Item 2', path: 'Subtema 2' },
-        { name: 'Item 3', path: 'Subtema 3' },
-        { name: 'Item 4', path: 'Subtema 4' },
-      ],
-    },
-    {
-      name: 'Integrales',
-      path: 'integrals',
-      routes: [{ name: 'Item 1', path: 'Subtema 1' }],
-    },
-  ]
+  const { routes } = data
 
   return (
     <div className="flex flex-row">
@@ -41,18 +12,17 @@ const Menu = () => {
         <div className="flex flex-col p-2">
           <p className="text-2xl text-[#ad0909]">Temas</p>
           <ul className={`list-none pl-4`}>
-            {routes.map(({ name, path, routes: subRoutes }) => (
-              <li
-                className={`text-nowrap font-medium hover:font-semibold text-gray-700 hover:text-indigo-500 transition duration-300 ease-in-out`}
-                key={name}
-              >
-                <Link to={path}>{name}</Link>
+            {routes?.map(({ name, path, routes: subRoutes }) => (
+              <li className={`text-nowrap transition duration-300 ease-in-out`} key={name}>
+                <Link className="font-medium hover:font-semibold text-gray-700 hover:text-indigo-500" to={path}>
+                  {name}
+                </Link>
 
                 {subRoutes && (
                   <ul className={`list-none pl-4 mt-1`}>
-                    {subRoutes.map(({ name: subName, path: subPath }) => (
+                    {subRoutes?.map(({ name: subName, path: subPath }) => (
                       <li className={`text-nowrap text-sm text-gray-500 hover:text-indigo-500 transition duration-300 ease-in-out`} key={subName}>
-                        <Link to={subPath}>{subName}</Link>
+                        <Link to={`${path}/${subPath}`}>{subName}</Link>
                       </li>
                     ))}
                   </ul>
