92713b6f5fdb2d6a6d0b3dcc0d102fb3d6de5b79
LuiFernandoZa <142746138+LuiFernandoZa@users.noreply.github.com>
2024-02-05 16:25:01 -0500

    colores para modo oscuro y claro

--- src/components/Integrales.jsx
+++ src/components/Integrales.jsx
@@ -1,7 +1,7 @@
 import style from '../styles/Integrales.module.css'
 export function Integrales() {
   return (
-    <div className={style.container}>
+    <div className={`${style.container} bg-[#ece5d1] dark:bg-[#2D2E40]`}>
       <h1>integrales</h1>
     </div>
   )
