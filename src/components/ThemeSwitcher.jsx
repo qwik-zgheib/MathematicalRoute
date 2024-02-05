8051f52d2cef008eed772d2d898fd2534eab8272
qwik-zgheib <qwik-zgheib@users.noreply.github.com>
2024-02-05 10:51:54 -0500

    use FontAwesomeIcon with faSun & faMoon

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import useDarkSide from '../hooks/useDarkSide'
 
 const ThemeSwitcher = () => {
   const [theme, toggleTheme] = useDarkSide()
 
   return (
-    <div className="flex items-center space-x-4">
+    <div className="flex items-center space-x-4 pr-3">
       <button className="text-gray-800 dark:text-gray-300 px-4 py-2 rounded" onClick={toggleTheme}>
-        {theme === 'dark' ? 'Light' : 'Dark'}
+        <FontAwesomeIcon icon={theme === 'dark' ? faSun : faMoon} className="ml-2" />
       </button>
     </div>
   )
