889daffe43eb21b0d3b53aef4385c43b95b74e1f
qwik-zgheib <qwik-zgheib@users.noreply.github.com>
2024-02-05 20:54:43 -0500

    change routes.json with helpers function

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,11 @@
 import { Link, Outlet } from 'react-router-dom'
+
+import { createRoutes } from '../helpers'
+
 import styles from '../styles/Navbar.module.css'
 
-import data from '../db/routes.json'
-
 const Menu = () => {
-  const { routes } = data
+  const routes = createRoutes()
 
   return (
     <div className="flex flex-row">
@@ -12,7 +13,7 @@ const Menu = () => {
         <div className="flex flex-col p-2">
           <p className="text-2xl text-[#ad0909]">Temas</p>
           <ul className={`list-none pl-4`}>
-            {routes?.map(({ name, path, routes: subRoutes }) => (
+            {Object.values(routes).map(({ name, path, routes: subRoutes }) => (
               <li className={`text-nowrap transition duration-300 ease-in-out`} key={name}>
                 <Link className="font-medium hover:font-semibold text-gray-700 hover:text-indigo-500" to={path}>
                   {name}
@@ -20,7 +21,7 @@ const Menu = () => {
 
                 {subRoutes && (
                   <ul className={`list-none pl-4 mt-1`}>
-                    {subRoutes?.map(({ name: subName, path: subPath }) => (
+                    {subRoutes.map(({ name: subName, path: subPath }) => (
                       <li className={`text-nowrap text-sm text-gray-500 hover:text-indigo-500 transition duration-300 ease-in-out`} key={subName}>
                         <Link to={`${path}/${subPath}`}>{subName}</Link>
                       </li>
