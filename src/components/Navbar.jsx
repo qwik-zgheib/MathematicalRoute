f895a4efab52a4383eade06b5f2debb957cc94dd
LuiFernandoZa <142746138+LuiFernandoZa@users.noreply.github.com>
2024-02-08 00:42:25 -0500

    se agrego una navegacion al navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,22 +4,43 @@ import styles from '../styles/Navbar.module.css'
 import Title from '../components/Title'
 import { SearchInput } from './SearchInput'
 import ThemeSwitcher from './ThemeSwitcher'
-
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
       <div className={`${styles.container} bg-[#011526] dark:bg-[#011526]`}>
-        <div>
-          <img className={styles.image} src="/src/assets/images/logoxaturn.png" />
-        </div>
+        <div className={styles.config}>
+          <div>
+            <img className={styles.image} src="/src/assets/images/logoxaturn.png" />
+          </div>
 
-        <div className="title">
-          <Title />
-        </div>
+          <div className="title">
+            <Title />
+          </div>
 
-        <div>
-          <SearchInput />
-          <ThemeSwitcher />
+          <div>
+            <SearchInput />
+            <ThemeSwitcher />
+          </div>
+        </div>
+        <div className={styles.navegar}>
+          <ul className={styles.lista}>
+            <Link to="/functions">
+              <li className={styles.subl}>Funciones</li>
+            </Link>
+
+            <Link to="/limits">
+              <li className={styles.subl}>Limites</li>
+            </Link>
+
+            <Link to="/derivatives">
+              <li className={styles.subl}>Derivadas</li>
+            </Link>
+
+            <Link to="/integrals">
+              <li className={styles.subl}>Integrales</li>
+            </Link>
+          </ul>
         </div>
       </div>
 
