fe8735be9dff17fee7fa4ba6a764983b5241ac98
qwik-zgheib <qwik-zgheib@users.noreply.github.com>
2024-02-11 10:46:35 -0500

    add Functions component

---
+++ src/components/Functions.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+/*
+Funciones
+Definición y Conceptos Básicos
+Dominio y Rango de una Función
+  Criterio para el Calculo del Dominio y Rango
+Funciones Especiales
+Operaciones con Funciones
+Composición de Funciones
+Funciones: Inyectivas, Suryectivas y Biyectivas
+Funciones Crecientes, Decrecientes y Monotomas
+Función Inversa
+  Función Inversa de una Composición
+*/
+
+const Functions = () => {
+  return <div>This is a functions</div>
+}
+
+export default Functions
