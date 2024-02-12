9572e703591cbb420e1752e4c407a6b68a37b423
qwik-zgheib <qwik-zgheib@users.noreply.github.com>
2024-02-11 20:54:48 -0500

    SearchInput modify styles

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,17 +1,48 @@
+import React from 'react'
+
+import { useState, useRef, useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
-export const SearchInput = () => {
+const SearchInput = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const [inputValue, setInputValue] = useState('')
+  const inputRef = useRef(null)
+
+  const toggleSearch = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value)
+  }
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.style.width = `${inputRef.current.scrollWidth}px`
+    }
+  }, [inputValue])
+
   return (
-    <div>
-      <input
-        type="text"
-        className=" absolute right-[20vw] top-2.5 border bg-gray-100 hover:bg-gray-200 border-gray-300 rounded-full text-blue-900 outline-none"
-      />
+    <div className="relative">
+      {isOpen ? (
+        <input
+          ref={inputRef}
+          type="text"
+          className="border bg-[#b8b9c1] dark:bg-[#4c4e58] border-[#737684] dark:border-[#737684] rounded-full text-blue-900 dark:text-blue-200 outline-none pl-3 p-8 py-2 transition-all duration-300"
+          value={inputValue}
+          onChange={handleInputChange}
+          autoFocus
+        />
+      ) : null}
+
       <FontAwesomeIcon
         icon={faSearch}
-        className=" absolute top-2 right-[20vw] hover:bg-[#ad0909] hover:rounded-full p-2 text-yellow-700 hover:text-white cursor-pointer"
+        className="absolute top-0 right-0 mt-3 mr-3 text-[#4c4e58] hover:text-[#5a03d5] dark:text-[#b8b9c1] hover:dark:text-[#bea6ff] cursor-pointer hover:scale-150 hover:animate-spin"
+        onClick={toggleSearch}
       />
     </div>
   )
 }
+
+export default SearchInput
